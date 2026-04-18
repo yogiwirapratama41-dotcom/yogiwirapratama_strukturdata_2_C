df6e94f5036d6a1bb180722302df7c52d92ccd09
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2026-04-19 01:51:25 +1000

    Add files via upload

---
+++ Tugas1if-else.py
@@ -0,0 +1,17 @@
+# if
+# else
+# elif
+
+nama = input("masukkan nama")
+NIM = input("masukkan PIN NIM")
+
+if nama =="yogi" :
+    NIM =="1"
+    print("Hai saya yogi wira pratama")
+    print("Panggil aja Yogiboy")
+    print("NIM saya 25241085")
+else : 
+    print("Mohon maaf anda bukan Yogi")
+    print("Nama yang masukkan salah")
+    print("NIM anda tidak bisa diakses")    
+print("program selesai")
