5a423edace1201e1ca6cce28e6863df731a3fd53
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2026-04-19 01:52:10 +1000

    Add files via upload

---
+++ Tugas3if-else.py
@@ -0,0 +1,23 @@
+# minta user memasukkan angka.
+# jika angka negatif,minta user memasukkan angka positif
+
+# contoh:
+# masukkan angka : -3
+# harus positif!
+# masukkan angka:
+
+# jawab:
+
+
+while True :
+    angka =int(input("masukan angka yang anda mau"))
+    
+    if angka >= 23 :
+        print (f"niali angka:{angka}")
+        print ("Ya Allah Yearan-arann kenggawek siak")
+
+    else:
+         print("Angka harus positif!")
+         print("silahkan masukkan angka yg positif ")
+    
+    print (f"Thankyou{angka}")
